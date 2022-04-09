d34baf6b5c55cffa6e21fd3ffd9aa94feb94c006
Luiza N Gomes <37775707+luizangomes@users.noreply.github.com>
2022-04-09 15:56:37 -0300

    tirando as funções de editar usuário

--- javascript-part.js
+++ javascript-part.js
@@ -11,10 +11,9 @@ function getcurrentRows(currentPage){                    //página atual com os 
     return users.slice(start, end)                      //retorna a lista de usuários atuais
 }
 
-function deleteUser(id){
+function deleteUser(id){                    //deleta usuários e retorna a lista dos restantes após um usuário ser deletado
     users = users.filter(user => user.id !== id)
     document.getElementById(`${id}`).remove()
-    render()
 }
 
 function createCurrentUsersTable(currentPage){
@@ -23,8 +22,8 @@ function createCurrentUsersTable(currentPage){
         let userRow = document.createElement('tr')      //identificando a linha da tabela/usuário
         userRow.setAttribute('id', rows.id)
 
-        let userName = document.createElement('td')
-        userName.appendChild(document.createTextNode(`${rows.first_name} ${rows.last_name}`))
+        let userName = document.createElement('td')         //cria o elemento
+        userName.appendChild(document.createTextNode(`${rows.first_name} ${rows.last_name}`))       //add o seu respectivo valor, usado para gerar o texto
 
         let userEmail = document.createElement('td')
         userEmail.appendChild(document.createTextNode(rows.email))
@@ -32,16 +31,16 @@ function createCurrentUsersTable(currentPage){
         let userCreationDate = document.createElement('td')
         userCreationDate.appendChild(document.createTextNode(rows.created_at))
 
-        let editAndDelete = document.createElement('td')
-        editAndDelete.classList.add('action_buttons')
-        let deleteButton = createDeleteButton(rows.id)
+        let editAndDelete = document.createElement('td')        //coluna das funções
+        editAndDelete.classList.add('action_buttons')           //se encontra na classe de botões de ação, são diferentes por suas colorações
+        let deleteButton = createDeleteButton(rows.id)          //redirecionam às suas respectivas funções
         let editButton = createEditButton(rows.id)
 
-        editAndDelete.appendChild(editButton)
+        editAndDelete.appendChild(editButton)                   //como os dois estão em uma coluna só, são adicionados a essa coluna
         editAndDelete.appendChild(deleteButton)
 
         userRow.appendChild(userName)
-        userRow.appendChild(userEmail)
+        userRow.appendChild(userEmail)                          //add cada um à sua coluna
         userRow.appendChild(userCreationDate)
         userRow.appendChild(editAndDelete)
 
@@ -55,27 +54,32 @@ function createUsersTable(currentPage){
     while (tbody.hasChildNodes()){          //se há outras linhas da página anterior
         tbody.removeChild(tbody.lastChild) //remover as linhas anteriores
       }
-    createCurrentUsersTable(currentPage).forEach(row => tbody.appendChild(row))
+    createCurrentUsersTable(currentPage).forEach(row => tbody.appendChild(row))         //vai add cada linha à tabela de acordo com a página atual
 }
 
-function createEditButton(id){
+
+function editingUserData(){             //mensagem para mostrar que ainda não há como editar usuários
+    const message = "Opa! Ainda não há como editar usuários..."
+    alert(message)
+}
+
+function createEditButton(id){                  //botão para edição
     const editButton = document.createElement('button')
     editButton.classList.add('text_button', 'edit_button')
     editButton.appendChild(document.createTextNode('editar'))
-    editButton.addEventListener('click', ()=> deleteUser(user.id, page))
+    editButton.addEventListener('click', ()=> editingUserData())
     return editButton
 }
 
-function createDeleteButton(id){
-    const deleteButton = document.createElement('button')
-    deleteButton.classList.add('text_button', 'delete_button')
-    deleteButton.appendChild(document.createTextNode('excluir'))
-    deleteButton.setAttribute('type', 'button')
-    deleteButton.addEventListener('click', () => deleteUser(id, page))
+function createDeleteButton(id){                //botão para deletar elementos
+    const deleteButton = document.createElement('button')       //cria o elemento do botão
+    deleteButton.classList.add('text_button', 'delete_button')     //classe do botão
+    deleteButton.appendChild(document.createTextNode('excluir'))        //prcoura o botão com o texto 'excluir'
+    deleteButton.setAttribute('type', 'button')                 //tipo do atributo
+    deleteButton.addEventListener('click', () => deleteUser(id))        //função que ele deve fazer ao ser pressionado
+    render()
     return deleteButton
 }
-
-createUsersTable(currentPage)
 
 
 function render(){
@@ -154,4 +158,5 @@ function renderPagination(totalPages){          //render para a utilização de 
     }
 }
 
+createUsersTable(currentPage)
 render()
