9c8db616cf08a11cba6f66bd648b2586cf559415
Luiza N Gomes <37775707+luizangomes@users.noreply.github.com>
2022-04-08 23:23:18 -0300

    novo meio de conbstruir a tabela usando DOM

--- javascript-part.js
+++ javascript-part.js
@@ -4,28 +4,79 @@ var ROWS_PER_PAGE = 5;
 function getTotalPages(){
     return Math.ceil(users.length / ROWS_PER_PAGE) //quantidade de páginas
 }
+
 function getcurrentRows(currentPage){                    //página atual com os usuários respectivos de suas páginas
     const start = (currentPage - 1) * ROWS_PER_PAGE
     const end = start + ROWS_PER_PAGE
     return users.slice(start, end)                      //retorna a lista de usuários atuais
 }
-function createUsersTable(currentPage){
+
+function deleteUser(id){
+    users = users.filter(user => user.id !== id)
+    document.getElementById(`${id}`).remove()
+    render()
+}
+
+function createCurrentUsersTable(currentPage){
     var currentRows = getcurrentRows(currentPage)           //pega os usuários atuais
-    var k = '<tbody>'
-    for(i = 0;i < currentRows.length; i++){
-        k+= '<tr>';
-        k+= '<td>' + (currentRows[i].first_name + " " + currentRows[i].last_name) + '</td>';
-        k+= '<td>' + currentRows[i].email + '</td>';
-        k+= '<td>' + currentRows[i].created_at + '</td>';
-        k+= '<td>' + '<button class="text_button edit_button" type="button">editar</button>' + "\t" + 
-            '<button class="text_button delete_button" type="button">excluir</button>' +'</td>';
-        k+= '</tr>';
-    }
-    k+='</tbody>';
-    document.getElementById('tableUserData').innerHTML = k;
+    return currentRows.map(rows => {
+        let userRow = document.createElement('tr')      //identificando a linha da tabela/usuário
+        userRow.setAttribute('id', rows.id)
+
+        let userName = document.createElement('td')
+        userName.appendChild(document.createTextNode(`${rows.first_name} ${rows.last_name}`))
+
+        let userEmail = document.createElement('td')
+        userEmail.appendChild(document.createTextNode(rows.email))
+
+        let userCreationDate = document.createElement('td')
+        userCreationDate.appendChild(document.createTextNode(rows.created_at))
+
+        let editAndDelete = document.createElement('td')
+        editAndDelete.classList.add('action_buttons')
+        let deleteButton = createDeleteButton(rows.id)
+        let editButton = createEditButton(rows.id)
+
+        editAndDelete.appendChild(editButton)
+        editAndDelete.appendChild(deleteButton)
+
+        userRow.appendChild(userName)
+        userRow.appendChild(userEmail)
+        userRow.appendChild(userCreationDate)
+        userRow.appendChild(editAndDelete)
+
+        return userRow
+    })
+}
+
+const tbody = document.querySelector('tbody')
+
+function createUsersTable(currentPage){
+    while (tbody.hasChildNodes()){          //se há outras linhas da página anterior
+        tbody.removeChild(tbody.lastChild) //remover as linhas anteriores
+      }
+    createCurrentUsersTable(currentPage).forEach(row => tbody.appendChild(row))
+}
+
+function createEditButton(id){
+    const editButton = document.createElement('button')
+    editButton.classList.add('text_button', 'edit_button')
+    editButton.appendChild(document.createTextNode('editar'))
+    editButton.addEventListener('click', ()=> deleteUser(user.id, page))
+    return editButton
+}
+
+function createDeleteButton(id){
+    const deleteButton = document.createElement('button')
+    deleteButton.classList.add('text_button', 'delete_button')
+    deleteButton.appendChild(document.createTextNode('excluir'))
+    deleteButton.setAttribute('type', 'button')
+    deleteButton.addEventListener('click', () => deleteUser(id, page))
+    return deleteButton
 }
 
 createUsersTable(currentPage)
+
 
 function render(){
     const totalPages = getTotalPages()
@@ -39,6 +90,14 @@ function changePage(newPage){
     const totalPages = getTotalPages()
     if (newPage >= 1 && newPage <= totalPages){     //se a página existe
         currentPage = newPage
+        createUsersTable(currentPage)
+    }
+    if (newPage < 1){     //se a página é abaixo de 1
+        currentPage = totalPages    //passa para a última página
+        createUsersTable(currentPage)
+    }
+    if (newPage > totalPages){     //se a página é acima do total de páginas
+        currentPage = 1         //passa para a primeira página
         createUsersTable(currentPage)
     }
     render()            //atualiza depois dos comandos realizados
